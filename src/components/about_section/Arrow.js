aee059674c327de45ed0ad4485001272af27efb4
Bartezky <83951494+Bartezky@users.noreply.github.com>
2024-08-27 22:32:55 +0200

    arrow not arrowing where is no need

--- src/components/about_section/Arrow.js
+++ src/components/about_section/Arrow.js
@@ -9,14 +9,13 @@ const Arrow = ({ gridRow, gridColumn, isVertical }) => {
         justifySelf: 'center',
         width: '30px',
         height: '30px',
-        margin: '10px 0 10px 0',
+        margin: '10px 0',
         backgroundColor: '#D3D3D3',
         clipPath: isVertical
-            ? 'polygon(0 0, 100% 50%, 0 100%)'
+            ? 'polygon(50% 100%, 100% 0%, 0% 0%)' // Zmieniony kształt dla pionowej strzałki
             : gridColumn === '1'
-                ? 'polygon(0 0, 100% 50%, 0 100%)'
-                : 'polygon(100% 0, 0 50%, 100% 100%)',
-        transform: isVertical ? 'rotate(90deg)' : 'none',
+                ? 'polygon(0 0, 100% 50%, 0 100%)' // Strzałka w lewo
+                : 'polygon(100% 0, 0 50%, 100% 100%)', // Strzałka w prawo
     };
 
     return (
